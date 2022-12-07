44ba6e6c07d8bf0f18296a1229bfdd3d9d6522c2
CarlosHugoRodriguezC <cahuroca.aw@gmail.com>
2022-12-07 01:29:58 -0600

    Products basic structure started

--- api-prototype/src/products/entities/product.entity.ts
+++ api-prototype/src/products/entities/product.entity.ts
@@ -1,4 +1,34 @@
-import { Schema } from "@nestjs/mongoose";
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { ValidProductStatus } from '../interfaces';
 
 @Schema()
-export class Product {}
+export class Product {
+  @Prop({ type: String, required: true })
+  name: string;
+
+  @Prop({ type: String })
+  description: string;
+
+  @Prop({ type: String })
+  shortDescription: string;
+
+  @Prop({ type: String })
+  tags: string[];
+
+  @Prop({ type: String })
+  lifeStyle: string;
+
+  @Prop({ type: String, unique: true })
+  SKU: string;
+
+  @Prop({ type: String, unique: true})
+  slug: string
+
+  // Todo image field
+  // Todo category, subcategory and departement fields
+
+  @Prop({ type: String, default: ValidProductStatus.Draft, enum:ValidProductStatus })
+  status: string;
+}
+
+export const ProductSchema = SchemaFactory.createForClass(Product);
